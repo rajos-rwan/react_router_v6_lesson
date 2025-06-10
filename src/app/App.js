4ad36755536daac554d394c137fe7a28d5aa9858
Rajos <rajos_rwan@hotmail.com>
2025-06-10 14:32:40 +0100

    Lesson complete

--- src/app/App.js
+++ src/app/App.js
@@ -19,8 +19,14 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path="about" element={ <About/> } />
     <Route path="sign-up" element={ <SignUp/> } />
     <Route path="articles" element={ <Articles/> } />
-    <Route path="categories" element={ <Categories/> } />
-    <Route path="profile" element={ <Profile/> } />
+    <Route path="articles/:title" element={ <Article/> } />
+    <Route path="authors/:name" element={ <Author/> } />
+    <Route path="categories" element={ <Categories/> }>
+      <Route path=":name" element={ <Category/> } />
+    </Route>
+    <Route path="profile" element={ <Profile/> }>
+      <Route path="edit" element={ <EditProfileForm/> } />
+    </Route>
   </Route>
 ))
 
